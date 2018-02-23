f9d359147d30bb688b0d029a4f35563c1fa07981
Paul Chong <paulhyunchong@gmail.com>
2018-02-22 21:25:04 -0800

    add 404 and login routes

--- notes-app-client/src/Routes.js
+++ notes-app-client/src/Routes.js
@@ -1,8 +1,14 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
+
 import Home from "./containers/Home/Home";
+import Login from "./containers/Login/Login";
+import NotFound from "./containers/NotFound/NotFound";
+
 
 export default () =>
   <Switch>
     <Route path="/" exact component={Home} />
+    <Route path="/login" exact component={Login} />
+    <Route component={NotFound} />
   </Switch>;
