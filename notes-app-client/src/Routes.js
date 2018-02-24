03f9db87f754fbbb86fa2d5a0b23310f5ff82b45
Paul Chong <paulhyunchong@gmail.com>
2018-02-24 14:06:57 -0800

    allow user to create notes

--- notes-app-client/src/Routes.js
+++ notes-app-client/src/Routes.js
@@ -4,6 +4,8 @@ import { Route, Switch } from "react-router-dom";
 import AppliedRoute from "./components/AppliedRoute";
 import Home from "./containers/Home/Home";
 import Login from "./containers/Login/Login";
+import Signup from "./containers/Signup/Signup";
+import NewNote from "./containers/NewNote/NewNote";
 import NotFound from "./containers/NotFound/NotFound";
 
 
@@ -11,5 +13,7 @@ export default ({ childProps }) =>
   <Switch>
     <AppliedRoute path="/" exact component={Home} props={ childProps } />
     <AppliedRoute path="/login" exact component={Login} props={ childProps } />
+    <AppliedRoute path="/signup" exact component={Signup} props={ childProps } />
+    <AppliedRoute path="/notes/new" exact component={NewNote} props={ childProps } />
     <Route component={NotFound} />
   </Switch>;
